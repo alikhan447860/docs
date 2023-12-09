1eda9ba0c164510cfe5d682faadc6189229ae3a9
Ali khan <khanali6861@gmail.com>
2023-12-09 15:59:54 +0530

    new react app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='w-full h-screen bg-zinc-800 relative'>
+      <h1 className='text-[13vw] leading-none tracking-tighter absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] font-semibold text-zinc-200'>ALI.</h1>
+    </div>
+  )
+}
+
+export default App
